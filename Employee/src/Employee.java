eaf38e8aeeba2c52006564665f3cf1d58a6d6e67
JIT13CS <kanhasingh50@gmail.com>
2021-10-19 12:37:30 +0530

    Employye Jave Commited

---
+++ Employee/src/Employee.java
@@ -0,0 +1,30 @@
+
+public class Employee {
+	private int emp_id;
+	private String FName;
+	private String Lname;
+	public int getEmp_id() {
+		return emp_id;
+	}
+	public void setEmp_id(int emp_id) {
+		this.emp_id = emp_id;
+	}
+	public String getFName() {
+		return FName;
+	}
+	public void setFName(String fName) {
+		FName = fName;
+	}
+	public String getLname() {
+		return Lname;
+	}
+	public void setLname(String lname) {
+		Lname = lname;
+	}
+	public Employee() {
+		//default constructor
+	}
+	
+	
+	
+}
